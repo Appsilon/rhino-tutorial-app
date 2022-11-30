aa27441a27957ea861ff14f94d8bd826bcb56a85
Ivan Millanes <ivanmillanes26@gmail.com>
2022-11-29 21:19:43 -0300

    Add rhino tutorial app

--- app/main.R
+++ app/main.R
@@ -1,13 +1,28 @@
+# app/main.R
+
 box::use(
-  shiny[bootstrapPage, moduleServer, NS, renderText, tags, textOutput],
+  shiny[bootstrapPage, div, h1, icon, moduleServer, NS, tags],
+)
+box::use(
+  app/view/chart,
+  app/view/table,
 )
 
 #' @export
 ui <- function(id) {
   ns <- NS(id)
+  
   bootstrapPage(
-    tags$h3(
-      textOutput(ns("message"))
+    h1("RhinoApplication"),
+    div(
+      class = "components-container",
+      table$ui(ns("table")),
+      chart$ui(ns("chart"))
+    ),
+    tags$button(
+      id = "help-button",
+      icon("question"),
+      onclick = "App.showHelp()"
     )
   )
 }
@@ -15,6 +30,11 @@ ui <- function(id) {
 #' @export
 server <- function(id) {
   moduleServer(id, function(input, output, session) {
-    output$message <- renderText("Hello!")
+    # Datasets are the only case when you need to use :: in `box`.
+    # This issue should be solved in the next `box` release.
+    data <- rhino::rhinos
+    
+    table$server("table", data = data)
+    chart$server("chart", data = data)
   })
 }
